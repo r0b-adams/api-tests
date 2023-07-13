c6be2e13f79c9234de6a010a440d7495135bd96d
Robert Adams <adamsiii.robert@gmail.com>
2023-07-12 22:22:12 -0700

    all thoughts done

--- test/m18.spec.js
+++ test/m18.spec.js
@@ -5,153 +5,168 @@ import { api } from '../utils/api.js';
 import { newUser } from '../utils/helpers.js';
 
 describe('M18 Social Network API', function () {
-  describe('User routes', function () {
-    /**
-     * Users
-     */
-    describe('GET /api/users', function () {
-      it('gets all users', async function () {
-        const { data } = await api.m18.users.getAll();
-        expect(data, "get all users didn't return an array").to.be.an('array');
+  // describe('User routes', function () {
+  //   /**
+  //    * Users
+  //    */
+  //   describe('GET /api/users', function () {
+  //     it('gets all users', async function () {
+  //       const { data } = await api.m18.users.getAll();
+  //       expect(data, "get all users didn't return an array").to.be.an('array');
+
+  //       if (data.length > 0) {
+  //         const [user] = data;
+  //         expect(user, 'missing key(s)').to.include.all.keys(
+  //           '_id',
+  //           'username',
+  //           'email',
+  //           'thoughts',
+  //           'friends',
+  //           'friendCount',
+  //         );
+  //       }
+  //     });
+  //   });
+
+  //   describe('GET /api/users/:userId', function () {
+  //     it('gets one user by id', async function () {
+  //       const { data: user } = await api.m18.users.post(newUser());
+  //       const { data: result } = await api.m18.users.getOne(user._id);
+
+  //       expect(result).to.be.an('object');
+  //       expect(result.username).to.equal(user.username);
+  //       expect(result.email).to.equal(user.email);
+  //     });
+  //   });
+
+  //   describe('POST /api/users', function () {
+  //     it('creates a user', async function () {
+  //       const user = newUser();
+  //       const { data: result } = await api.m18.users.post(user);
+
+  //       expect(result, 'missing key(s)').to.include.all.keys(
+  //         '_id',
+  //         'username',
+  //         'email',
+  //         'thoughts',
+  //         'friends',
+  //         'friendCount',
+  //       );
+  //       expect(result.username).to.equal(user.username);
+  //       expect(result.email).to.equal(user.email);
+  //     });
+  //   });
+
+  //   describe('PUT /api/users/:userId', function () {
+  //     it('updates a user by id', async function () {
+  //       const { data: user } = await api.m18.users.post(newUser());
+  //       const update = newUser();
+  //       const { data: result } = await api.m18.users.put(user._id, update);
+
+  //       expect(result).to.be.an('object');
+  //       expect(result.username).to.equal(update.username);
+  //       expect(result.email).to.equal(update.email);
+  //     });
+  //   });
+
+  //   describe('DELETE /api/users/:userId', function () {
+  //     it('deletes a user by id', async function () {
+  //       const { data: before } = await api.m18.users.getAll();
+  //       const { data: user } = await api.m18.users.post(newUser());
+  //       const { data: after } = await api.m18.users.getAll();
+
+  //       // check user was created
+  //       expect(after.length - before.length).to.equal(1);
+
+  //       // delete the user
+  //       await api.m18.users.delete(user._id);
+  //       const { data: result } = await api.m18.users.getAll();
+
+  //       // check user was removed
+  //       expect(result.length).to.equal(before.length);
+  //     });
+  //   });
+
+  //   describe('POST /api/users/:userId/friends/:friendId - ', function () {
+  //     it('adds a friend', async function () {
+  //       const { data: user } = await api.m18.users.post(newUser());
+  //       const { data: friend } = await api.m18.users.post(newUser());
+  //       await api.m18.users.addFriend(user._id, friend._id);
+  //       const { data: result } = await api.m18.users.getOne(user._id);
+
+  //       expect(result.friends.length).to.equal(1);
+  //       expect(result.friendCount).to.equal(1);
+  //     });
+  //   });
+
+  //   describe('DELETE /api/users/:userId/friends/:friendId', function () {
+  //     it('removes a friend', async function () {
+  //       const { data: user } = await api.m18.users.post(newUser());
+  //       const { data: friend } = await api.m18.users.post(newUser());
+  //       await api.m18.users.addFriend(user._id, friend._id);
+  //       const { data: added } = await api.m18.users.getOne(user._id);
+
+  //       expect(added.friends.length).to.equal(1);
+  //       expect(added.friendCount).to.equal(1);
+
+  //       await api.m18.users.removeFriend(user._id, friend._id);
+  //       const { data: result } = await api.m18.users.getOne(user._id);
+
+  //       expect(result.friends.length).to.equal(0);
+  //       expect(result.friendCount).to.equal(0);
+  //     });
+  //   });
+  // });
+
+  /**
+   * Thoughts
+   */
+  describe('Thought routes', function () {
+    describe('GET /api/thoughts', function () {
+      it('gets all thoughts', async function () {
+        const { data } = await api.m18.thoughts.getAll();
+        expect(data, "get all thoughts didn't return an array").to.be.an('array');
 
         if (data.length > 0) {
-          const [user] = data;
-          expect(user, 'missing key(s)').to.include.all.keys(
+          const [thought] = data;
+          expect(thought, 'missing key(s)').to.include.all.keys(
             '_id',
             'username',
-            'email',
-            'thoughts',
-            'friends',
-            'friendCount',
+            'thoughtText',
+            'createdAt',
+            'reactions',
+            'reactionCount',
           );
         }
       });
     });
 
-    describe('GET /api/users/:userId', function () {
-      it('gets one user by id', async function () {
-        const { data: user } = await api.m18.users.post(newUser());
-        const { data: result } = await api.m18.users.getOne(user._id);
+    // describe('GET /api/thoughts/:thoughtId', function () {
+    //   it('gets one thought', async function () {});
+    // });
 
-        expect(result).to.be.an('object');
-        expect(result.username).to.equal(user.username);
-        expect(result.email).to.equal(user.email);
-      });
-    });
+    // describe('POST /api/thoughts', function () {
+    //   it('creates a thought', async function () {});
+    //   it('adds created thought id to user thoughts array', async function () {});
+    // });
 
-    describe('POST /api/users', function () {
-      it('creates a user', async function () {
-        const user = newUser();
-        const { data: result } = await api.m18.users.post(user);
+    // describe('PUT /api/thoughts/:thoughtId', function () {
+    //   it('updates a thought', async function () {});
+    // });
 
-        expect(result, 'missing key(s)').to.include.all.keys(
-          '_id',
-          'username',
-          'email',
-          'thoughts',
-          'friends',
-          'friendCount',
-        );
-        expect(result.username).to.equal(user.username);
-        expect(result.email).to.equal(user.email);
-      });
-    });
+    // describe('DELETE /api/thoughts/:thoughtId', function () {
+    //   it('deletes a thought', async function () {});
+    //   it('removes delete thought id from user thoughts array', async function () {});
+    // });
 
-    describe('PUT /api/users/:userId', function () {
-      it('updates a user by id', async function () {
-        const { data: user } = await api.m18.users.post(newUser());
-        const update = newUser();
-        const { data: result } = await api.m18.users.put(user._id, update);
+    // describe('POST /api/thoughts/:thoughtId/reactions', async function () {
+    //   it('adds a reaction', function () {});
+    // });
 
-        expect(result).to.be.an('object');
-        expect(result.username).to.equal(update.username);
-        expect(result.email).to.equal(update.email);
-      });
-    });
-
-    describe('DELETE /api/users/:userId', function () {
-      it('deletes a user by id', async function () {
-        const { data: before } = await api.m18.users.getAll();
-        const { data: user } = await api.m18.users.post(newUser());
-        const { data: after } = await api.m18.users.getAll();
-
-        // check user was created
-        expect(after.length - before.length).to.equal(1);
-
-        // delete the user
-        await api.m18.users.delete(user._id);
-        const { data: result } = await api.m18.users.getAll();
-
-        // check user was removed
-        expect(result.length).to.equal(before.length);
-      });
-    });
-
-    describe('POST /api/users/:userId/friends/:friendId - ', function () {
-      it('adds a friend', async function () {
-        const { data: user } = await api.m18.users.post(newUser());
-        const { data: friend } = await api.m18.users.post(newUser());
-        await api.m18.users.addFriend(user._id, friend._id);
-        const { data: result } = await api.m18.users.getOne(user._id);
-
-        expect(result.friends.length).to.equal(1);
-        expect(result.friendCount).to.equal(1);
-      });
-    });
-
-    describe('DELETE /api/users/:userId/friends/:friendId', function () {
-      it('removes a friend', async function () {
-        const { data: user } = await api.m18.users.post(newUser());
-        const { data: friend } = await api.m18.users.post(newUser());
-        await api.m18.users.addFriend(user._id, friend._id);
-        const { data: added } = await api.m18.users.getOne(user._id);
-
-        expect(added.friends.length).to.equal(1);
-        expect(added.friendCount).to.equal(1);
-
-        await api.m18.users.removeFriend(user._id, friend._id);
-        const { data: result } = await api.m18.users.getOne(user._id);
-
-        expect(result.friends.length).to.equal(0);
-        expect(result.friendCount).to.equal(0);
-      });
-    });
+    // describe('DELETE /api/thoughts/:thoughtId/reactions/:reactionId', async function () {
+    //   it('removes a reaction', function () {});
+    // });
   });
-
-  /**
-   * Thoughts
-   */
-  // describe('Thought routes', function () {
-  //   describe('POST /api/thoughts', function () {
-  //     it('creates a thought', function () {});
-  //     it('adds created thought id to user thoughts array', function () {});
-  //   });
-
-  //   describe('GET /api/thoughts/:thoughtId', function () {
-  //     it('gets one thought', function () {});
-  //   });
-
-  //   describe('GET /api/thoughts', function () {
-  //     it('gets all thoughts', function () {});
-  //   });
-
-  //   describe('PUT /api/thoughts/:thoughtId', function () {
-  //     it('updates a thought', function () {});
-  //   });
-
-  //   describe('DELETE /api/thoughts/:thoughtId', function () {
-  //     it('deletes a thought', function () {});
-  //     it('removes delete thought id from user thoughts array', function () {});
-  //   });
-
-  //   describe('POST /api/thoughts/:thoughtId/reactions', function () {
-  //     it('adds a reaction', function () {});
-  //   });
-
-  //   describe('DELETE /api/thoughts/:thoughtId/reactions/:reactionId', function () {
-  //     it('removes a reaction', function () {});
-  //   });
-  // });
 
   /**
    * BONUS
